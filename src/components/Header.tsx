0bd668ffbfe272bc4635a65678bc320ef0053fb1
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2023-11-22 12:06:08 +0400

    start project

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
+
+// <a
+//         href="https://open.spotify.com/playlist/049zhJ0C72DDx7bzJwQN2Z?si=5c118e54477d414a&nd=1&dlsi=5faea799fa5548e7"
+//         target="_blank"
+//         rel="noreferrer noopener"
+//       >
+//         Header
+//       </a>
